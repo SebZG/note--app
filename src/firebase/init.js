e96e7dfda6671ffcbe76609b4a04a124c84e1237
SebZG <sebastian.zapata.g@gmail.com>
2024-03-03 16:04:56 +0000

    Clean up

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 // import { getAnalytics } from "firebase/analytics";
-// import 'dotenv/config';
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -10,14 +9,6 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-   // apiKey: "AIzaSyBgqlWoAjopiD2yt07Nz3gL8MoM3f8RrfM",
-   // authDomain: "fir-practice-fdfcc.firebaseapp.com",
-   // databaseURL: "https://fir-practice-fdfcc-default-rtdb.europe-west1.firebasedatabase.app",
-   // projectId: "fir-practice-fdfcc",
-   // storageBucket: "fir-practice-fdfcc.appspot.com",
-   // messagingSenderId: "1003953824668",
-   // appId: "1:1003953824668:web:02c92e5c81bf3c6fd45e59",
-   // measurementId: "G-95VGDFWDGT",
    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
    databaseURL: import.meta.env.VITE_REACT_APP_FIREBASE_DATABASE_URL,
