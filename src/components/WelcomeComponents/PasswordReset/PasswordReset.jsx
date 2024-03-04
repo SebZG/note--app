f665f43f34492ef75dbc5d4594703aa657d09487
SebZG <sebastian.zapata.g@gmail.com>
2024-03-04 07:27:08 +0000

    Fixed missing styling

--- src/components/WelcomeComponents/PasswordReset/PasswordReset.jsx
+++ src/components/WelcomeComponents/PasswordReset/PasswordReset.jsx
@@ -2,7 +2,7 @@ import './PasswordReset.css'
 
 const PasswordReset = ({ handlePasswordReset }) => {
    return (
-      <p onClick={handlePasswordReset} className="text-center">forgot password?</p>
+      <p onClick={handlePasswordReset} className="forgot-password text-center">forgot password?</p>
    );
 };
 
